6aa609eee96b8eeaf8c2741f6c7aabc653c7f723
zahoor <zahoorghani2@gmail.com>
2022-01-04 12:34:24 +0500

    committing project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnetmvcdummy.github.io.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
